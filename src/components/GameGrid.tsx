6254763489eb089778b90fa329fc65a28b262c60
josepucciarelli <126281528+josepucciarelli@users.noreply.github.com>
2023-06-21 15:11:16 -0400

    rafactor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,27 +13,26 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GAmeCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GAmeCardContainer>
-          ))}
-        {data.map((game) => (
-          <GAmeCardContainer key={game.id}>
-            <GAmeCard game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GAmeCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GAmeCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GAmeCardContainer key={game.id}>
+          <GAmeCard game={game} />
+        </GAmeCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
