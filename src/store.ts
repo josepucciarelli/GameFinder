c4d4d56f622a488b3338e633119260fc89289db9
josepucciarelli <126281528+josepucciarelli@users.noreply.github.com>
2023-07-27 14:04:29 -0400

    Fix a bug in fetching games

--- src/store.ts
+++ src/store.ts
@@ -18,8 +18,12 @@ interface GameQueryStore {
 const useGameQueryStore = create<GameQueryStore>(set => ({
   gameQuery: {},
   setSearchText: (searchText) => set(store => ({ gameQuery: { searchText } })),
-  setGenreId: (genreId) => set(store => ({ gameQuery: { ...store.gameQuery, genreId}})),
-  setPlatformId: (platformId) => set(store => ({ gameQuery: { ...store.gameQuery, platformId }})),
+  setGenreId: (genreId) => set(store => ({ gameQuery: { ...store.gameQuery, genreId, searchText: undefined}})),
+  setPlatformId: (platformId) => set(store => ({ gameQuery: {
+    ...store.gameQuery,
+    platformId,
+    searchText: undefined,
+  }})),
   setSortOrder: (sortOrder) => set(store => ({ gameQuery: {...store.gameQuery, sortOrder }}))
 }))
 
